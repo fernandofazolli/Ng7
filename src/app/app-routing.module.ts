c28d874b2ddb68acd1e723ed53d9d88df954f98b
fernandofazolli <fernandofazolli@gmail.com>
2019-08-02 12:27:28 -0300

    Projeto basico

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    HttpClientModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  firstClick() {
+    console.log('Clicado no service');
+  }
+
+  getUsers() {
+    return this.http.get('http://localhost:3000/users');
+  }
+}
